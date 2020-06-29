374b505f04858a856953d05c00d9028ce844f420
Scorpion <Shazebhussaini1994@gmail.com>
2020-06-29 17:58:07 +0530

    Create go-installer.sh

---
+++ go-installer.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [[ -z "$GOPATH" ]];then
+echo "It looks like go is not installed, would you like to install it now"
+PS3="Please select an option : "
+choices=("yes" "no")
+select choice in "${choices[@]}"; do
+        case $choice in
+                yes)
+
+                                        echo "Installing Golang"
+                                        wget https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz
+                                        sudo tar -xvf go1.13.4.linux-amd64.tar.gz
+                                        sudo mv go /usr/local
+                                        export GOROOT=/usr/local/go
+                                        export GOPATH=$HOME/go
+                                        export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+                                        echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+                                        echo 'export GOPATH=$HOME/go'   >> ~/.bashrc
+                                        echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
+                                        source ~/.bashrc
+                                        sleep 1
+                                        break
+                                        ;;
+                                no)
+                                        echo "Please install go and rerun this script"
+                                        echo "Aborting installation..."
+                                        exit 1
+                                        ;;
+        esac
+done
+fi
