4fd47f2e5275ea972cf8c2ad21e8b7f2525c5509
Shazeb01 <shazebvps@gmail.com>
2020-08-12 02:27:15 -0700

    vim-go

--- go-installer.sh
+++ go-installer.sh
@@ -11,3 +11,5 @@ echo 'export GOROOT=/usr/local/go' >> ~/.zshrc
 echo 'export GOPATH=$HOME/go'   >> ~/.zshrc
 echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.zshrc
 source ~/.zshrc
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone https://github.com/fatih/vim-go.git ~/.vim/plugged/vim-go
